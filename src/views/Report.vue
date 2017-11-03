68f8f5bd6d41985f0a5297ee9e497566c3b70f4e
Diamond <1015678620@qq.com>
2017-11-03 12:57:16 +0800

    。

---
+++ src/views/Report.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div style="font-size:26px">健康小贴士:</div>
+    <div  v-for="item in msg">
+      <div v-html="item" class="pt-1"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import bus from '../assets/eventBus.js'
+export default {
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  mounted () {
+    let tvm = this
+    bus.$on('reportmsg', function (msg) {
+      tvm.msg = msg
+    })
+  }
+}
+</script>
+
