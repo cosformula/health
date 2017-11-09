2302e8f1771937b6bcb3a9b6602f629eb63c030f
Diamond <1015678620@qq.com>
2017-11-09 22:59:26 +0800

    。

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -75,7 +75,7 @@
         <v-card-text>{{dialogtext10}}</v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1"  @click.native="dialog10 = false">取消</v-btn>
+          <v-btn color="green darken-1"  @click.native="dialog10 = false">确认</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
